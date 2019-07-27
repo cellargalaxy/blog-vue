4356b8e318017838b962afc24937cee0b0fc023f
cellargalaxy <cellargalaxy@gmail.com>
2019-07-27 18:09:10 +0800

    boot配置不提交到git上

--- install.sh
+++ install.sh
@@ -1,4 +1,35 @@
 #!/bin/bash
+while :
+do
+    read -p "please enter git url(required):" gitUrl
+    if [ ! -z $gitUrl ];then
+        break
+    fi
+done
+read -p "please enter git ref(default:master):" ref
+if [ -z $ref ];then
+    ref="master"
+fi
+read -p "please enter username(optional):" username
+if [ -z $username ];then
+    username=""
+fi
+read -p "please enter password(optional):" password
+if [ -z $password ];then
+    password=""
+fi
+read -p "please enter flushTime(default:600000:10min):" flushTime
+if [ -z $flushTime ];then
+    flushTime="600000"
+fi
+echo '{
+  "gitUrl": "'+$gitUrl+'",
+  "ref": "'+$ref+'",
+  "username": "'+$username+'",
+  "password": "'+$password+'",
+  "flushTime": '+$flushTime+',
+  "repositoryPath": "static/repository"
+}' > bootConfig.json
 rm -rf node_modules
 rm -rf .nuxt
 npm i
