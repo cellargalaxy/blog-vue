8e1b973415bc0bb2d24b6e549570ceb952ba1f4c
cellargalaxy <cellargalaxy@gmail.com>
2019-12-04 17:41:59 +0800

    备份

--- generate.sh
+++ generate.sh
@@ -2,14 +2,15 @@
 if [ -z $sleepTime ];then
     sleepTime="3600"
 fi
+nohup node ./node_modules/http-server/bin/http-server -g -c$sleepTime >http.log 2>&1 &
 node git_clone_pull.js clone
 while :
 do
     node git_clone_pull.js copyStatusFile
     npm run generate
     node git_clone_pull.js removeStatusFile
-    node git_clone_pull.js remove out
-    node git_clone_pull.js copy dist out
+    node git_clone_pull.js remove public
+    node git_clone_pull.js copy dist public
     sleep $sleepTime
     node git_clone_pull.js pull
 done
--- install-docker.sh
+++ install-docker.sh
@@ -14,7 +14,7 @@ read -p "please enter git username(default:''):" GIT_USERNAME
 if [ -z $GIT_USERNAME ];then
     GIT_USERNAME=""
 fi
-read -p "please enter git password(default:''):" GIT_PASSWORD
+read -s "please enter git password(default:''):" GIT_PASSWORD
 if [ -z $GIT_PASSWORD ];then
     GIT_PASSWORD=""
 fi
@@ -22,11 +22,16 @@ read -p "please enter sleepTime(default:3600):" sleepTime
 if [ -z $sleepTime ];then
     sleepTime="3600"
 fi
+read -p "please enter listen port(default:8888):" listenPort
+if [ -z $listenPort ];then
+    listenPort="8888"
+fi
 
 echo 'git url: '$GIT_URL
 echo 'git ref: '$GIT_REF
 echo 'git username: '$GIT_USERNAME
 echo 'sleepTime: '$sleepTime
+echo 'listenPort: '$listenPort
 echo 'input any key go on,or control+c over'
 read
 
@@ -35,6 +40,6 @@ docker build -t blog_vue .
 echo 'docker create volume'
 docker volume create blog_vue
 echo 'docker run'
-docker run -d --restart=always --name blog_vue -v blog_vue:/src/out -e GIT_URL=$GIT_URL -e GIT_REF=$GIT_REF -e GIT_USERNAME=$GIT_USERNAME -e GIT_PASSWORD=$GIT_PASSWORD -e sleepTime=$sleepTime blog_vue
+docker run -d --restart=always --name blog_vue -p $listenPort:8080 -v blog_vue:/src/public -e GIT_URL=$GIT_URL -e GIT_REF=$GIT_REF -e GIT_USERNAME=$GIT_USERNAME -e GIT_PASSWORD=$GIT_PASSWORD -e sleepTime=$sleepTime blog_vue
 
 echo 'all finish'
