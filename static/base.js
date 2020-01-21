525b709f0f567310cf376d9bf8f47755e0c33a40
cellargalaxy <cellargalaxy@gmail.com>
2020-01-21 17:48:38 +0800

    添加流程图表，数学公式，大图点开和代码块行数显示

--- static/base.js
+++ static/base.js
@@ -1,12 +1,27 @@
 window.onload = () => {
+  bashWindowOnload()
+  try {
+    if (staticWindowOnload && typeof (staticWindowOnload) == "function") {
+      staticWindowOnload()
+    }
+  } catch (e) {
+    console.log('staticWindowOnload function does not exist')
+  }
+}
+
+function bashWindowOnload() {
+  tableClass()
+  codeRow()
+}
+
+function tableClass() {
   document.querySelectorAll('table').forEach((table) => {
     table.classList.add('table', 'b-table', 'table-striped', 'table-hover', 'table-responsive')
   })
-  try {
-    if (customizeWindowOnload && typeof (customizeWindowOnload) == "function") {
-      customizeWindowOnload()
-    }
-  } catch (e) {
-    console.log('customizeWindowOnload function does not exist')
-  }
+}
+
+function codeRow() {
+  document.querySelectorAll('code').forEach((code) => {
+    code.innerHTML = "<ol><li>" + code.innerHTML.replace(/\n/g, "\n</li><li>") + "\n</li></ol>"
+  })
 }
