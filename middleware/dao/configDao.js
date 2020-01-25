6a48b705b0ec4f45d1af08ffcc2585ef2daaf76e
cellargalaxy <cellargalaxy@gmail.com>
2020-01-25 17:32:16 +0800

    图片轮播

--- middleware/dao/configDao.js
+++ middleware/dao/configDao.js
@@ -5,12 +5,9 @@ import log from '../utils/log'
 import fileIO from '../utils/fileIO'
 
 const logger = log('configDao')
-const repositoryPath = config.repositoryPath
-const configName = 'config.json'
+const configPath = config.configPath
 
 function getConfig() {
-  const configPath = fileIO.join(repositoryPath, configName)
-  logger.info('创建配置文件路径,configPath: {}', configPath)
   if (!fileIO.isFile(configPath)) {
     logger.error('仓库配置文件不存在或者不是文件')
     return null
