58e2b318d68981d2e320e18407fb6654c4e6ba49
cellargalaxy <cellargalaxy@gmail.com>
2020-08-06 14:58:01 +0800

    修改归档path为archive

---
+++ components/archives.vue
@@ -0,0 +1,64 @@
+<template>
+  <b-card no-body class="white-background-8" style="border: none">
+    <br/>
+    <el-timeline>
+      <el-timeline-item :key="i" :timestamp="archive.dateString+' ('+archive.files.length+')'"
+                        placement="top" v-for="(archive,i) in archives">
+        <b-card :key="j" class="transparent" no-body style="border:none;" v-for="(file,j) in archive.files">
+          <b-link :href="file.url">{{file.title}}</b-link>
+          <p v-if="file.dateString">({{file.dateString}})</p>
+        </b-card>
+      </el-timeline-item>
+    </el-timeline>
+  </b-card>
+</template>
+
+<archives :archives="archives"/>
+
+<script>
+  export default {
+    name: "archives",
+    props: {
+      archives: {
+        default() {
+          return [
+            {
+              "dateString": "2020-01",
+              "files": [
+                {"title": "测试文章标题-1-1", "url": "#", "dateString": "2020-01-01"},
+                {"title": "测试文章标题-1-2", "url": "#", "dateString": "2020-01-02"},
+                {"title": "测试文章标题-1-3", "url": "#", "dateString": "2020-01-03"},
+              ],
+            },
+            {
+              "dateString": "2020-02",
+              "files": [
+                {"title": "测试文章标题-2-1", "url": "#", "dateString": "2020-02-01"},
+                {"title": "测试文章标题-2-2", "url": "#", "dateString": "2020-02-02"},
+                {"title": "测试文章标题-2-3", "url": "#", "dateString": "2020-02-03"},
+              ],
+            },
+            {
+              "dateString": "2020-03",
+              "files": [
+                {"title": "测试文章标题-3-1", "url": "#", "dateString": "2020-03-01"},
+                {"title": "测试文章标题-3-2", "url": "#", "dateString": "2020-03-02"},
+                {"title": "测试文章标题-3-3", "url": "#", "dateString": "2020-03-03"},
+              ],
+            },
+          ]
+        }
+      },
+    },
+  }
+</script>
+
+<style scoped>
+  .transparent {
+    background-color: rgba(255, 255, 255, 0);
+  }
+
+  .white-background-8 {
+    background-color: rgba(255, 255, 255, 0.8);
+  }
+</style>
---
+++ pages/archives.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <navbar :config="navbarConfig"/>
+
+    <b-container>
+      <br/>
+      <page-head :config="homeConfig"/>
+      <br/>
+      <archives-component :archives="archives"/>
+    </b-container>
+    <br/>
+    <page-foot :buildTimeString="buildTimeString" :config="pageFootConfig"/>
+    <backtop/>
+  </div>
+</template>
+
+<script>
+  import navbar from '../components/navbar'
+  import pageHead from '../components/pageHead'
+  import archivesComponent from '../components/archives'
+  import pageFoot from '../components/pageFoot'
+  import backtop from '../components/backtop'
+
+  import fileService from '../middleware/service/fileService'
+  import configService from '../middleware/service/configService'
+  import utils from "../middleware/utils/utils"
+
+  export default {
+    name: "archives",
+    async asyncData({error}) {
+      const navbarConfig = configService.getNavbarConfig()
+      if (!navbarConfig) {
+        const errorConfig = configService.getErrorPageConfig("404")
+        error(errorConfig)
+        return
+      }
+      const homeConfig = configService.getHomeConfig()
+      if (!homeConfig) {
+        const errorConfig = configService.getErrorPageConfig("404")
+        error(errorConfig)
+        return
+      }
+      const pageFootConfig = configService.getPageFootConfig()
+      if (!pageFootConfig) {
+        const errorConfig = configService.getErrorPageConfig("404")
+        error(errorConfig)
+        return
+      }
+      const timeLines = fileService.getTimeLines()
+      if (!timeLines || timeLines.length == 0) {
+        return
+      }
+      for (let i = 0; i < timeLines.length; i++) {
+        for (let j = 0; j < timeLines[i].files.length; j++) {
+          timeLines[i].files[j] = {
+            title: timeLines[i].files[j].title,
+            url: timeLines[i].files[j].url,
+            dateString: timeLines[i].files[j].dateString
+          }
+        }
+      }
+      return {
+        navbarConfig: navbarConfig,
+        homeConfig: homeConfig,
+        archives: timeLines,
+        pageFootConfig: pageFootConfig,
+        buildTimeString: utils.formatDate(new Date(), pageFootConfig.buildTimeFormat),
+      }
+    },
+    components: {
+      navbar,
+      pageHead,
+      archivesComponent,
+      pageFoot,
+      backtop,
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
