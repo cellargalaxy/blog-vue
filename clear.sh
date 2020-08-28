06486f253db5a55e16b7570a7fd1a1881f91d5e5
cellargalaxy <cellargalaxy@gmail.com>
2020-08-28 21:32:26 +0800

    sh脚本换行符

--- clear.sh
+++ clear.sh
@@ -1,6 +1,1 @@
-#!/usr/bin/env bash
-rm -rf repository
-rm -rf dist
-rm -rf public
-rm -rf public-back
-rm -f staticFileData.json
+#!/usr/bin/env bashrm -rf repositoryrm -rf distrm -rf publicrm -rf public-backrm -f staticFileData.json
