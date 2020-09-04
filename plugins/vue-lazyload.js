c25e619d5b6ff806f467cde2694f57b1d3533fe2
cellargalaxy <cellargalaxy@gmail.com>
2020-09-04 11:21:44 +0800

    添加图片加载中和加载失败图标

--- plugins/vue-lazyload.js
+++ plugins/vue-lazyload.js
@@ -1,7 +1,11 @@
 import Vue from "vue"
 import VueLazyload from "vue-lazyload"
+import error from "../assets/images/error.svg"
+import loading from "../assets/images/loading.svg"
 
 Vue.use(VueLazyload, {
   preLoad: 1,
-  attempt: 1
+  attempt: 1,
+  error: error,
+  loading: loading,
 })
