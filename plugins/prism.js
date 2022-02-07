c613a897526dbfbbd1afd67999cac3d4554001fa
lixinzhao <lixinzhao@bytedance.com>
2022-02-07 22:06:02 +0800

    1

--- plugins/prism.js
+++ plugins/prism.js
@@ -13,26 +13,17 @@ import "prismjs/plugins/line-numbers/prism-line-numbers.css"
 import "prismjs/plugins/line-highlight/prism-line-highlight"
 import "prismjs/plugins/line-highlight/prism-line-highlight.css"
 
-// Include some other plugins: (optional)
-import "prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard"
-import "prismjs/plugins/highlight-keywords/prism-highlight-keywords"
-import "prismjs/plugins/show-language/prism-show-language"
-
 // Include additional languages
 import "prismjs/components/prism-bash.js"
-import "prismjs/components/prism-cpp.js"
 import "prismjs/components/prism-css.js"
 import "prismjs/components/prism-csv.js"
-import "prismjs/components/prism-flow.js"
 import "prismjs/components/prism-go.js"
 import "prismjs/components/prism-java.js"
 import "prismjs/components/prism-javascript.js"
 import "prismjs/components/prism-json.js"
 import "prismjs/components/prism-latex.js"
 import "prismjs/components/prism-markdown.js"
-import "prismjs/components/prism-matlab.js"
 import "prismjs/components/prism-sql.js"
-import "prismjs/components/prism-xml-doc.js"
 import "prismjs/components/prism-yaml.js"
 
 // Set vue SFC to markdown
