0f68d2aa7e73e4ec10b87cb4dc53cfc5bb02b081
cellargalaxy <cellargalaxy@gmail.com>
2022-03-19 01:48:57 +0800

    备份

--- plugins/lazysizes.js
+++ plugins/lazysizes.js
@@ -1,2 +1,4 @@
 // https://derkinzi.de/optimized-responsive-lazyloading-images-with-nuxt/
 require('lazysizes')
+
+$('.nuxt-content').find('img').addClass('lazyload')
--- plugins/medium-zoom.js
+++ plugins/medium-zoom.js
@@ -2,6 +2,8 @@
 import Vue from 'vue'
 import zoom from 'medium-zoom'
 
+$('.nuxt-content').find('img').addClass('zoom')
+
 const initZoom = () => {
   zoom('img.zoom:not(.medium-zoom-image)', {background: 'rgba(0, 0, 0, 0.6)'})
 }
