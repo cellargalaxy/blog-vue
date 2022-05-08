3d93dc5f3537e315cdee02fd4c7546b46e591e71
cellargalaxy <cellargalaxy@users.noreply.github.com>
2022-05-08 14:28:19 +0000

    deploy: dcfe4e618a6cba390a390c6867cbd15ab2dc481b

---
+++ _nuxt/static/1652020089/test/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fblog-vue\u002F_nuxt\u002Fstatic\u002F1652020089",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftest",config:{_app:{basePath:"\u002Fblog-vue\u002F",assetsPath:"\u002Fblog-vue\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"dc3f6b86"}}}}(null));
