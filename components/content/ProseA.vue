835577dc500fcf94eb224de7af8d44e5e54a5860
cellargalaxy <cellargalaxy@gmail.com>
2023-04-08 16:19:53 +0800

    备份

---
+++ components/content/ProseA.vue
@@ -0,0 +1,25 @@
+<template>
+    <NuxtLink :to="href" :target="target">
+        <slot/>
+    </NuxtLink>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    href: {
+        type: String,
+        default: ''
+    },
+    target: {
+        type: String,
+        default: undefined,
+        required: false
+    }
+})
+</script>
+
+<style scoped>
+a {
+    background-color: #0047e1;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    extends: '../typography',
     modules: [
         '@nuxtjs/tailwindcss',
         '@element-plus/nuxt',
